8e829157a4723ff8cf90f41fd78ca8f4b48290c8
Pascal <Pascal@DESKTOP-P4R5757>
2021-05-19 20:16:22 +0200

    command works, inserted more rest req

--- src/index.js
+++ src/index.js
@@ -7,37 +7,33 @@ const {connectToDesk} = require("./startup/deskHandler");
 const {scanForDesk} = require("./startup/deskHandler");
 
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 
-app.get('/', async(req, res) => {
-    res.send(true)
-    const address = await scanForDesk() // handle reject
-
+app.post('/connect', async(req, res) => {
+    const address = await scanForDesk().catch((err)=> {
+        res.send(err)
+    });
     if(!!address) {
         console.log(`Returned address is ${address}`);
         await connectToDesk(address);
-        await startDeskServer()
-        await sleep(1000)
-        console.log(await sendCommand({ op: "moveTo", pos: 14 }, true)); // wait is a bool
-
-        setTimeout(()=> {
-            stopDeskServer()
-        },100000)
-
+        await startDeskServer();
+        res.send(true);
     }
-    res.send()
+    res.send("Error: No address found");
+});
+app.post('/disconnect', async(req, res) => {
+    await stopDeskServer().catch((err) => res.send(err));
+    res.send(true);
 });
 
 
-app.post('/move/:position', async(req, res) => {
- /*    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    winston.info("Position: ", req.body,  "posted")
-*/
 
-    res.send(true);
+
+app.post('/move/:position', async(req, res) => {
+    const hasMoved = await sendCommand({ op: "moveTo", pos: 14 }, true); // wait is a bool
+    res.send(hasMoved);
 });
 
 app.listen(port, () => {
