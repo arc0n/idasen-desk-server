ddc2ca5831a1c65ac87f4dd21705af38ff1ff5a9
Pascal <Pascal@DESKTOP-P4R5757>
2021-05-19 19:58:29 +0200

    insert sleep before first command

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {sleep} = require("./control/utils");
 const {sendCommand} = require("./startup/deskHandler");
 const {stopDeskServer} = require("./startup/deskHandler");
 const {startDeskServer} = require("./startup/deskHandler");
@@ -11,12 +12,14 @@ const port = 3000
 
 
 app.get('/', async(req, res) => {
-    const address = await scanForDesk() // handle reject
     res.send(true)
+    const address = await scanForDesk() // handle reject
+
     if(!!address) {
         console.log(`Returned address is ${address}`);
         await connectToDesk(address);
         await startDeskServer()
+        await sleep(1000)
         await sendCommand({ op: "moveTo", pos: 14 }, false); // wait promise?
 
         setTimeout(()=> {
