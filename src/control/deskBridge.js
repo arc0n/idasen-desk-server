7f766e9ffb9bfaf1a7c7ca85012a7147ed4e97ad
Pascal <Pascal@DESKTOP-P4R5757>
2021-05-29 14:40:22 +0200

    catching error attempt -7

--- src/control/deskBridge.js
+++ src/control/deskBridge.js
@@ -126,7 +126,7 @@ class DeskBridge extends EventEmitter {
 
       this.didUpdateDevice();
     } else {
-      this.log("Discovered a desk at", peripheral.address);
+      this.log("Discovered another desk at", peripheral.address);
     }
   }
 
